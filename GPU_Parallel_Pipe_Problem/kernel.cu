200648f2dea900bc6d6d26c8766d57bcb5572e0e
jack <jj21065@gmail.com>
2018-01-09 11:35:39 +0800

    Add gpu parallel project

--- GPU_Parallel_Pipe_Problem/kernel.cu
+++ GPU_Parallel_Pipe_Problem/kernel.cu
@@ -1,121 +1,243 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-
+#include <malloc.h>
+#include <time.h>
+#include <stdlib.h>
 #include <stdio.h>
-
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
-
-__global__ void addKernel(int *c, const int *a, const int *b)
+#include <math.h>
+#define N 29
+float *Q;
+float *d_Q;
+void Allocate_Memory(int n)
 {
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
+	size_t size = n*sizeof(float);
+
+	Q = (float*)malloc(size);
+	cudaError_t error = cudaMalloc((void**)&d_Q, size);
+	printf("Allocate mem : %s\n", cudaGetErrorString(error));
 }
 
+void Free_Memory()
+{
+	if (Q)
+		free(Q);
+	cudaError_t error = cudaFree(d_Q);
+	printf("Free mem : %s\n", cudaGetErrorString(error));
+}
+
+void CopyMemToDevice(float *data, int n)
+{
+	/*for (int i = 0; i < n; i++)
+	{
+	R[i] = data[i];
+	}*/
+	size_t size = n*sizeof(float);
+	cudaError_t error = cudaMemcpy(d_Q, Q, size, cudaMemcpyHostToDevice);
+	printf("Memcpy Host to Device : %s\n", cudaGetErrorString(error));
+}
+
+void CopyMemToHost(float *data, int n)
+{
+	cudaError_t error = cudaMemcpy(Q, d_Q, n*sizeof(float), cudaMemcpyDeviceToHost);
+	printf("Memcpy Device to Host : %s\n", cudaGetErrorString(error));
+	/*for (int i = 0; i < n; i++)
+	{
+	data[i] = R[i];
+	}*/
+}
+__global__ void Compute_Q(float* Q, int n);
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+	
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
+	Allocate_Memory(N);
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+	Q[0] = sqrt(10.0 / rs0);
+	Q[1] = Q[0];
+	Q[2] = Q[0];
+	Q[3] = 0;
+	Q[4] = 0;
+	Q[5] = 0;
+	Q[6] = 0;
+	Q[7] = 0;
+	Q[8] = 0;
+	Q[9] = 0;
+	Q[10] = 0;
+	Q[11] = 0;
+	Q[12] = 0;
+	Q[13] = 0;
+	Q[14] = Q[0];
+	Q[15] = 0;
+	Q[16] = 0;
+	Q[17] = 0;
+	Q[18] = 0;
+	Q[19] = 0;
+	Q[20] = 0;
+	Q[21] = 0;
+	Q[22] = 0;
+	Q[23] = 0;
+	Q[24] = 0;
+	Q[25] = 0;
+	Q[26] = 0;
+	Q[27] = Q[0];
+	Q[28] = 0;
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
+	CopyMemToDevice(Q, N);
 
-    return 0;
+	
+
+	// initial conditions 
+
+
+	int n = 2;
+	int iter_no = 5000;
+	int i;
+	clock_t t1 = clock();
+	printf("inital Q[0] = %g\n", Q[0]);
+	for (i = 0; i < iter_no; i++)
+	{
+	
+	}
+	clock_t t2 = clock();
+
+	printf("time consume : %f", t2 - t1);
+	//	printf("abs = %g\n",myabs(-2));
+	printf("final Q[0] = %g\n", Q[0]);
+	printf("final Q9-EA = %g\n", Q[27]);
+	printf("final Qh-EB = %g\n", Q[28]);
+
+	system("pause");
+	Free_Memory();
+	return 0;
 }
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
+__global__ void Compute_Q(float* pQ,int n )
 {
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
+	int i = blockDim.x*blockIdx.x + threadIdx.x;
+	// pipe parameter 
+	float r01 = 1;
+	float r16 = 5;
+	float r12 = 1;
+	float r03 = 5;
+	float r34 = 3;
 
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
+	float r24 = 1;
+	float r25 = 1;
+	float r56 = 3;
+	float r3e = 1;
+	float r47 = 1;
 
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	float r57 = 1;
+	float r78 = 2;
+	float r89 = 3;
+	float r69 = 3;
 
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	float r8a = 1;
+	float reb = 2;
+	float rdg = 5;
+	float ref = 5;
 
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	float rab = 3;
+	float rbc = 2;
+	float r9g = 1;
+	float rcf = 2;
 
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+	float rad = 2;
+	float rdc = 1;
+	float rgh = 1;
+	float rfh = 2;
 
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+	float rs0 = 1;
+	float r9EA = 1;
+	float rhEB = 1;
 
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
+	float R[11] = { 0 };
 
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
+	if (i < n)
+	{
+		/// calculate adjust flow pQ
+		switch (i)
+		{
+		case 0:
+			if (2 * (r01*abs(pQ[1]) + r12*abs(pQ[3]) + r24*abs(pQ[6]) + r34*abs(pQ[5]) + r03*abs(pQ[4])) > 0)
+				R[0] = -(r01*pQ[1] * abs(pQ[1]) + r12*pQ[3] * abs(pQ[3]) + r24*pQ[6] * abs(pQ[6]) - r34*pQ[5] * abs(pQ[5]) - r03*pQ[4] * abs(pQ[4])) / (2 * (r01*abs(pQ[1]) + r12*abs(pQ[3]) + r24*abs(pQ[6]) + r34*abs(pQ[5]) + r03*abs(pQ[4])));
+			break;
+		case 1:
+			if (2 * (r16*abs(pQ[2]) + r56*abs(pQ[8]) + r25*abs(pQ[7]) + r12*abs(pQ[3])) > 0)
+				R[1] = -(r16*pQ[2] * abs(pQ[2]) - r56*pQ[8] * abs(pQ[8]) - r25*pQ[7] * abs(pQ[7]) - r12*pQ[3] * abs(pQ[3])) / (2 * (r16*abs(pQ[2]) + r56*abs(pQ[8]) + r25*abs(pQ[7]) + r12*abs(pQ[3])));
+			break;
+		case 2:
+			if (2 * (r34*abs(pQ[5]) + r47*abs(pQ[10]) + r78*abs(pQ[12]) + r8a*abs(pQ[15]) + rab*abs(pQ[19]) + reb*abs(pQ[16]) + r3e*abs(pQ[9])) > 0)
+				R[2] = -(r34*pQ[5] * abs(pQ[5]) + r47*pQ[10] * abs(pQ[10]) + r78*pQ[12] * abs(pQ[12]) + r8a*pQ[15] * abs(pQ[15]) + rab*pQ[19] * abs(pQ[19]) - reb*pQ[16] * abs(pQ[16]) - r3e*pQ[9] * abs(pQ[9])) / (2 * (r34*abs(pQ[5]) + r47*abs(pQ[10]) + r78*abs(pQ[12]) + r8a*abs(pQ[15]) + rab*abs(pQ[19]) + reb*abs(pQ[16]) + r3e*abs(pQ[9])));
+			break;
+		case 3:
+			if (2 * (r56*abs(pQ[8]) + r69*abs(pQ[14]) + r89*abs(pQ[13]) + r78*abs(pQ[12]) + r57*abs(pQ[11])) > 0)
+				R[3] = -(r56*pQ[8] * abs(pQ[8]) + r69*pQ[14] * abs(pQ[14]) - r89*pQ[13] * abs(pQ[13]) - r78*pQ[12] * abs(pQ[12]) - r57*pQ[11] * abs(pQ[11])) / (2 * (r56*abs(pQ[8]) + r69*abs(pQ[14]) + r89*abs(pQ[13]) + r78*abs(pQ[12]) + r57*abs(pQ[11])));
+			break;
+		case 4:
+			if (2 * (r89*abs(pQ[13]) + r9g*abs(pQ[21]) + rdg*abs(pQ[17]) + rad*abs(pQ[23]) + r8a*abs(pQ[15])) > 0)
+				R[4] = -(r89*pQ[13] * abs(pQ[13]) - r9g*pQ[21] * abs(pQ[21]) - rdg*pQ[17] * abs(pQ[17]) - rad*pQ[23] * abs(pQ[23]) - r8a*pQ[15] * abs(pQ[15])) / (2 * (r89*abs(pQ[13]) + r9g*abs(pQ[21]) + rdg*abs(pQ[17]) + rad*abs(pQ[23]) + r8a*abs(pQ[15])));
+			break;
+		case 5:
+			if (2 * (rad*abs(pQ[23]) + rdc*abs(pQ[24]) + rbc*abs(pQ[20]) + rab*abs(pQ[19])) > 0)
+				R[5] = -(rad*pQ[23] * abs(pQ[23]) + rdc*pQ[24] * abs(pQ[24]) - rbc*pQ[20] * abs(pQ[20]) - rab*pQ[19] * abs(pQ[19])) / (2 * (rad*abs(pQ[23]) + rdc*abs(pQ[24]) + rbc*abs(pQ[20]) + rab*abs(pQ[19])));
+			break;
+		case 6:
 
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+			if (2 * (reb*abs(pQ[16]) + rbc*abs(pQ[20]) + rcf*abs(pQ[22]) + ref*abs(pQ[18])) > 0)
+				R[6] = -(reb*pQ[16] * abs(pQ[16]) + rbc*pQ[20] * abs(pQ[20]) + rcf*pQ[22] * abs(pQ[22]) - ref*pQ[18] * abs(pQ[18])) / (2 * (reb*abs(pQ[16]) + rbc*abs(pQ[20]) + rcf*abs(pQ[22]) + ref*abs(pQ[18])));
+			break;
+		case 7:
+			if (2 * (rdg*abs(pQ[17]) + rgh*abs(pQ[25]) + rfh*abs(pQ[26]) + rcf*abs(pQ[22]) + rdc*abs(pQ[24])) > 0)
+				R[7] = -(rdg*pQ[17] * abs(pQ[17]) + rgh*pQ[25] * abs(pQ[25]) - rfh*pQ[26] * abs(pQ[26]) - rcf*pQ[22] * abs(pQ[22]) - rdc*pQ[24] * abs(pQ[24])) / (2 * (rdg*abs(pQ[17]) + rgh*abs(pQ[25]) + rfh*abs(pQ[26]) + rcf*abs(pQ[22]) + rdc*abs(pQ[24])));
+			break;
+		case 8:
+			if (2 * (r25*abs(pQ[7]) + r57*abs(pQ[11]) + r47*abs(pQ[10]) + r24*abs(pQ[6])) > 0)
+				R[8] = -(r25*pQ[7] * abs(pQ[7]) + r57*pQ[11] * abs(pQ[11]) - r47*pQ[10] * abs(pQ[10]) - r24*pQ[6] * abs(pQ[6])) / (2 * (r25*abs(pQ[7]) + r57*abs(pQ[11]) + r47*abs(pQ[10]) + r24*abs(pQ[6])));
+			break;
+		case 9:
+			if (2 * (rs0*abs(pQ[0]) + r01*abs(pQ[1]) + r16*abs(pQ[2]) + r69*abs(pQ[14]) + r9EA*abs(pQ[27])) > 0)
+				R[9] = -(rs0*pQ[0] * abs(pQ[0]) + r01*pQ[1] * abs(pQ[1]) + r16*pQ[2] * abs(pQ[2]) + r69*pQ[14] * abs(pQ[14]) + r9EA*pQ[27] * abs(pQ[27]) - 10) / (2 * (rs0*abs(pQ[0]) + r01*abs(pQ[1]) + r16*abs(pQ[2]) + r69*abs(pQ[14]) + r9EA*abs(pQ[27])));
+			break;
+		case 10:
+			if (2 * (rs0*abs(pQ[0]) + r03*abs(pQ[4]) + r3e*abs(pQ[9]) + ref*abs(pQ[18]) + rfh*abs(pQ[26]) + rhEB*abs(pQ[28])) > 0)
+				R[10] = -(rs0*pQ[0] * abs(pQ[0]) + r03*pQ[4] * abs(pQ[4]) + r3e*pQ[9] * abs(pQ[9]) + ref*pQ[18] * abs(pQ[18]) + rfh*pQ[26] * abs(pQ[26]) + rhEB*pQ[28] * abs(pQ[28]) - 10) / (2 * (rs0*abs(pQ[0]) + r03*abs(pQ[4]) + r3e*abs(pQ[9]) + ref*abs(pQ[18]) + rfh*abs(pQ[26]) + rhEB*abs(pQ[28])));
+			break;
 
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
+		}
+	
+		/// add the adjust flow to each pQ pipes
+		pQ[1] = pQ[1] + R[0] + R[9];
+		pQ[3] = pQ[3] + R[0] - R[1];
+		pQ[6] = pQ[6] + R[0] - R[8];
+		pQ[5] = pQ[5] - R[0] + R[2];
+		pQ[4] = pQ[4] - R[0] + R[10];
+		pQ[2] = pQ[2] + R[1] + R[9];
+		pQ[8] = pQ[8] - R[1] + R[3];
+		pQ[7] = pQ[7] - R[1] + R[8];
+		pQ[10] = pQ[10] - R[8] + R[2];
+		pQ[12] = pQ[12] + R[2] - R[3];
+		pQ[15] = pQ[15] + R[2] - R[4];
+
+		pQ[11] = pQ[11] - R[3] + R[8];
+		pQ[13] = pQ[13] - R[3] + R[4];
+		pQ[14] = pQ[14] + R[3] + R[9];
+		pQ[23] = pQ[23] - R[4] + R[5];
+		pQ[21] = pQ[21] - R[4];
+		pQ[17] = pQ[17] - R[4] + R[7];
+		pQ[24] = pQ[24] + R[5] - R[7];
+		pQ[25] = pQ[25] + R[7];
+		pQ[26] = pQ[26] - R[7] + R[10];
+		pQ[28] = pQ[28] + R[10];
+		pQ[27] = pQ[27] + R[9];
+		pQ[19] = pQ[19] + R[2] - R[5];
+		pQ[16] = pQ[16] - R[2] + R[6];
+		pQ[9] = pQ[9] - R[2] + R[10];
+		pQ[18] = pQ[18] - R[6] + R[10];
+		pQ[20] = pQ[20] - R[5] + R[6];
+		pQ[22] = pQ[22] + R[6] - R[7];
+		pQ[0] = pQ[0] + R[10] + R[9];
+
+	}
 }
